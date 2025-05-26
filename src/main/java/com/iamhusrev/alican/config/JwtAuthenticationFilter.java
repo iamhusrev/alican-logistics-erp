1e94edcaf61068e8636c9bdfdd6bb32a71546be0
iamhusrev <iamhusrev@gmail.com>
2025-05-26 17:20:46 +0300

    Refactor application structure by renaming controllers to follow API conventions, update application properties for Thymeleaf configuration, and enhance security settings for resource access. Add necessary dependencies for Thymeleaf and webjars in pom.xml.

--- src/main/java/com/iamhusrev/alican/config/JwtAuthenticationFilter.java
+++ src/main/java/com/iamhusrev/alican/config/JwtAuthenticationFilter.java
@@ -39,7 +39,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
         String path = request.getServletPath();
-        return path.startsWith("/auth/") || path.startsWith("/login/");
+        return path.startsWith("/auth") || path.startsWith("/dashboard");
     }
 
     @Override
